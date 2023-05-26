2edda441edd42f77d2052e6e764c6d4eafa5290d
John Darrah <132414407+johndarrah@users.noreply.github.com>
2023-05-26 12:05:19 -0700

    Update [DATAPALS-4436]  BItcoin Email SLA by interval.sql

--- Analysis/2023/[DATAPALS-4436]  BItcoin Email SLA by interval.sql
+++ Analysis/2023/[DATAPALS-4436]  BItcoin Email SLA by interval.sql
@@ -9,74 +9,67 @@
 WITH
   handled_agg AS (
     SELECT
-      TIME_SLICE(e.touch_time, 30, 'minute')                AS touch_30_ts
+      TIME_SLICE(e.touch_time, 30, 'minute')                   AS touch_30_ts_utc
+      , CONVERT_TIMEZONE('America/Los_Angeles', 'UTC', e.touch_time) AS touch_30_ts_pst
       , e.queue_name
       , e.queue_id
-      , q.communication_channel                           AS channel
-      , CASE
-          WHEN s.country_code != 'US' AND s.country_code IS NOT NULL
-            THEN 'GLOBAL'
-          ELSE q.team_name
-        END                                               AS vertical
-      , q.business_unit_name
-      , COUNT(DISTINCT touch_id)                          AS handled_volume
+      , COUNT(DISTINCT touch_id)                                     AS handled_volume
       , COUNT(DISTINCT CASE
                          WHEN touch_type = 'EML' OR touch_type = 'EML/STS'
                            THEN touch_id
                          ELSE NULL
-                       END)                               AS email_handled
+                       END)                                          AS email_handled
       , COUNT(DISTINCT CASE
                          WHEN (touch_type = 'TRN' OR touch_type = 'EML/TRN')
                            THEN touch_id
                          ELSE NULL
-                       END)                               AS transfer_handled
+                       END)                                          AS transfer_handled
       , COUNT(DISTINCT CASE
                          WHEN touch_type = 'STS'
                            THEN touch_id
                          ELSE NULL
-                       END)                               AS resolved_handled
+                       END)                                          AS resolved_handled
       , COUNT(DISTINCT CASE
                          WHEN touch_type = 'TR' OR touch_type = 'EML/TR'
                            THEN touch_id
                          ELSE NULL
-                       END)                               AS bulk_handled
+                       END)                                          AS bulk_handled
       , COUNT(DISTINCT CASE
                          WHEN touch_type IN ('EML', 'EML/TRN', 'TR', 'EML/STS', 'EML/TR')
                            THEN touch_id
                          ELSE NULL
-                       END)                               AS response_handled
+                       END)                                          AS response_handled
       , SUM(CASE
               WHEN touch_type IN ('EML', 'EML/TRN', 'TR', 'EML/STS', 'EML/TR')
                 THEN response_time_hours
               ELSE 0
-            END)                                          AS total_response_time
-      , SUM(touch_handle_time)                            AS total_handle_time
-      , total_response_time / NULLIF(response_handled, 0) AS art
-      , total_handle_time / NULLIF(handled_volume, 0)     AS aht
+            END)                                                     AS total_response_time
+      , SUM(touch_handle_time)                                       AS total_handle_time
+      , total_response_time / NULLIF(response_handled, 0)            AS art
+      , total_handle_time / NULLIF(handled_volume, 0)                AS aht
       , COUNT(DISTINCT CASE
                          WHEN (response_time_hours <= 24 AND touch_type != 'TRN')
                            THEN touch_id
                          ELSE NULL
-                       END)                               AS handled_in_sl
+                       END)                                          AS handled_in_sl
     FROM app_cash_cs.public.email_touches AS e
     LEFT JOIN app_cash_cs.public.support_cases AS s
       ON e.case_id = s.case_id
-    LEFT JOIN app_datamart_cco.archive.team_queue_catalog_all_sfdc AS q
-      ON q.queue_id = e.queue_id
     WHERE
-      YEAR(touch_30_ts) = 2023
+      YEAR(touch_30_ts_utc) = 2023
       AND e.queue_name IN ('CS Crypto')
-    GROUP BY 1, 2, 3, 4, 5, 6
+    GROUP BY 1, 2, 3, 4
   )
 SELECT
-  touch_30_ts
+  touch_30_ts_utc
+--   , touch_30_ts_pst
   , queue_name
   , SUM(response_handled)                                          AS response_volume
+  , SUM(handled_in_sl)                                             AS handled_volume
   , NVL(SUM(handled_in_sl) / NULLIFZERO(SUM(response_handled)), 0) AS percent_in_sla
 FROM handled_agg
 WHERE
-  touch_30_ts IS NOT NULL
-  AND YEAR(touch_30_ts) = 2023
+  touch_30_ts_utc IS NOT NULL
   AND response_handled > 0
 GROUP BY 1, 2
 ORDER BY 1 DESC
