196104c4ba6f3b3ee6f9cbebdc629b87907d1195
John Darrah <132414407+johndarrah@users.noreply.github.com>
2023-06-05 16:37:32 -0700

    Update [DATAPALS-4371] Seeking Detailed Handle Time Data by Case.sql

--- analysis/2023/[DATAPALS-4371] Seeking Detailed Handle Time Data by Case.sql
+++ analysis/2023/[DATAPALS-4371] Seeking Detailed Handle Time Data by Case.sql
@@ -4,10 +4,12 @@ SELECT
   , ecd.ldap_today
   , ecd.manager
   , ecd.managers_manager
-  , tbu.start_timestamp                                 AS touch_start_time
-  , tbu.stop_timestamp                                  AS touch_end_time
-  , tbu.time_spent                                      AS handle_time_seconds
-  , SUM(tbu.time_spent) OVER (PARTITION BY tbu.case_id) AS total_handle_time_seconds
+  , tbu.start_timestamp                                               AS touch_start_time
+  , tbu.stop_timestamp                                                AS touch_end_time
+  , TIME_SLICE(tbu.start_timestamp, 30, 'MINUTE')                     AS touch_start_interval
+  , DATEADD(MINUTE, 30, TIME_SLICE(tbu.stop_timestamp, 30, 'MINUTE')) AS touch_end_interval
+  , tbu.time_spent                                                    AS handle_time_seconds
+  , SUM(tbu.time_spent) OVER (PARTITION BY tbu.case_id)               AS total_handle_time_seconds
   , sc.problem_tag
 FROM app_cash_cs.cfone.time_by_users tbu
 JOIN app_cash_cs.public.support_cases sc
@@ -21,3 +23,8 @@ QUALIFY
   ROW_NUMBER() OVER (PARTITION BY ecd.employee_id ORDER BY created_date DESC) = 1 -- latest record
 ORDER BY tbu.start_timestamp
 ;
+
+SELECT
+  touch_start_time
+  , TIME_SLICE(touch_start_time, 30, 'MINUTE') AS interval_timestamp
+FROM app_cash_cs.public.email_touches
