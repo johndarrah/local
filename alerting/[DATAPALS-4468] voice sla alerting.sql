c75aec9b1998610cd5e4d8577c835c356ea32ac8
John Darrah <132414407+johndarrah@users.noreply.github.com>
2023-07-07 10:28:53 -0700

    update src

--- alerting/[DATAPALS-4468] voice sla alerting.sql
+++ alerting/[DATAPALS-4468] voice sla alerting.sql
@@ -11,24 +11,25 @@
 -- callbacks are missing employee information
 
 SELECT
-  TO_CHAR(
-    DATE_TRUNC(HOURS,
-               CONVERT_TIMEZONE('America/Los_Angeles', 'UTC', cr.call_start_time)
-      ),
-    'YYYY-MM-DD HH24:MI:SS')                                          AS entering_hour
-  , ecd.employee_id
-  , ecd.full_name
-  , ecd.city
-  , CASE
-      WHEN cr.queue_name IN ('US-EN Cash CS Manager Escalations',
-                             'US-EN Cash CS Manager Escalation',
-                             'US-EN Cash Concierge',
-                             'US-EN Manager Escalations',
-                             'US-ES Manager Escalations')
-        THEN 'ERET'
-      ELSE 'CORE CS'
-    END                                                               AS vertical
-  , 'Voice'                                                           AS channel
+  1 = 1
+  --   TO_CHAR(
+  --     DATE_TRUNC(HOURS,
+  --                CONVERT_TIMEZONE('America/Los_Angeles', 'UTC', cr.call_start_time)
+  --       ),
+  --     'YYYY-MM-DD HH24:MI:SS')                                          AS entering_hour
+  --   , ecd.employee_id
+  --   , ecd.full_name
+  --   , ecd.city
+  --   , CASE
+  --       WHEN cr.queue_name IN ('US-EN Cash CS Manager Escalations',
+  --                              'US-EN Cash CS Manager Escalation',
+  --                              'US-EN Cash Concierge',
+  --                              'US-EN Manager Escalations',
+  --                              'US-ES Manager Escalations')
+  --         THEN 'ERET'
+  --       ELSE 'CORE CS'
+  --     END                                                               AS vertical
+  --   , 'Voice'                                                           AS channel
   , SUM(cr.speed_to_callback / 60)                                    AS response_time_min
   , COUNT(IFF(cr.speed_to_callback IS NOT NULL, cr.contact_id, NULL)) AS count_of_callbacks
   , SUM(cr.handle_time) / 60                                          AS handle_time_min
@@ -99,10 +100,14 @@ SELECT
   , touches_in_sl / qualified_sla_touches                             AS sl_percent
   , response_time_min / NULLIFZERO(qualified_sla_touches)             AS avg_response_time_min -- denominator: why touches in sla and not touches handles
   , handle_time_min / NULLIFZERO(touches_handled)                     AS avg_handle_time_min   -- denominator: why touches handled and not touches in SLA
+  , AVG(cr.handle_time) / 60                                          AS avg_handle_time_min_v2
+  , AVG(cr.speed_to_callback) / 60                                    AS avg_response_time_min_v2
+  , response_time_min / count(DISTINCT IFF(cr.requested_callback, cr.contact_id, NULL))
+
 FROM app_cash_cs.preprod.call_records cr
 LEFT JOIN app_cash_cs.public.employee_cash_dim ecd
   ON ecd.amazon_connect_id = cr.agent_user_name
   AND CONVERT_TIMEZONE('America/Los_Angeles', 'UTC', cr.call_start_time)::DATE BETWEEN ecd.start_date AND ecd.end_date
 WHERE
   CONVERT_TIMEZONE('America/Los_Angeles', 'UTC', cr.call_start_time)::DATE >= '2022-01-01'
-GROUP BY 1, 2, 3, 4, 5, 6
+-- GROUP BY 1, 2, 3, 4, 5, 6
