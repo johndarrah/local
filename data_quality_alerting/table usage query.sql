5af68ac323d2539deda971b995d42bcc9d0995f7
John Darrah <132414407+johndarrah@users.noreply.github.com>
2023-09-28 15:27:48 -0700

    Update table usage query.sql

--- data_quality_alerting/table usage query.sql
+++ data_quality_alerting/table usage query.sql
@@ -4,17 +4,18 @@ SELECT DISTINCT
   , query_text
   , MAX(end_date) AS query_last_run_ds
 FROM app_datamart_cco.data_pipelines.snowflake_query_history
+  -- from snowflake_usage.account_usage.query_history
 WHERE
   1 = 1
   --   date filters
   AND YEAR(end_date) = 2023
   --   not owned by Datapals
   AND LOWER(user_name) NOT IN ('anomalo', 'mellisor', 'mfazza', 'lakehouse',
-                               'app_cash_3pr', 'app_datamart_cco',
-                               'fivetran', 'V1_EDGE_COLLIBRA', 'DMC_PII')
+                               'app_cash_3pr', 'app_datamart_cco', 'fivetran', 'v1_edge_collibra',
+                               'dmc_pii', 'app_cash_cs')
   --   database.schema.table filter
-  AND query_text ILIKE '%app_cash_cs.public.tiktok_inapp_cases%'
+  AND query_text ILIKE '%app_cash_cs.public.support_cases%'
   --   column filter
-  AND query_text ILIKE '%summary_of_action_take%'
-GROUP BY 1,2,3
+  AND query_text ILIKE '%notes%'
+GROUP BY 1, 2, 3
 ORDER BY 1 DESC
