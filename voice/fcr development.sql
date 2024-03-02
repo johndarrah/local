0a24ef987b18cb23cd4c09932c66305a3d63f5d9
John Darrah <132414407+johndarrah@users.noreply.github.com>
2024-03-01 16:29:34 -0800

    Update fcr development.sql

--- voice/fcr development.sql
+++ voice/fcr development.sql
@@ -1,43 +1,27 @@
 WITH
   base AS (
     SELECT DISTINCT
-      sc.customer_id
-      , sc.customer_token
+      sc.customer_token
       , sc.problem_tag
-      , sc.case_creation_date_time
+      , CONVERT_TIMEZONE('America/Los_Angeles', 'UTC', sc.case_creation_date_time) AS case_creation_ts_utc
       , sc.case_id
       , sc.origin
-    -- , cr.customer_endpoint
-    -- , cr.is_customer
-    -- , cr.is_voice_pii_passed
-    -- , cr.voice_pii_customer_token
-    -- , sc.problem_type
-    -- , sc.selected_category
-    -- , sc.amazon_connect_contact_id
-    -- , sc.channel
-    -- , sc.mapped_queue
-    -- , sc.vertical
-    -- , sc.contact_id
-    -- , cr.case_customer_token
     FROM app_cash_cs.public.support_cases sc
     JOIN app_cash_cs.preprod.call_records cr
       ON cr.case_id = sc.case_id
-      AND YEAR(cr.call_start_time_utc) = 2024
     WHERE
       1 = 1
       AND YEAR(sc.case_creation_date_time) = 2024
       AND sc.customer_token IS NOT NULL
-    -- AND cr.customer_endpoint = '+++ts3Nrw3HBFO0oHFSvrHN9wnQ4tqYMIt/oVfd/zyw='
     -- AND sc.customer_token = 'C_002210y39'
   )
   , new_issue_contact AS (
     SELECT DISTINCT
       b1.case_id
-      , DATEDIFF(DAY, b1.case_creation_date_time, b2.case_creation_date_time) AS days_since_new_issue_contact
-      , b2.case_creation_date_time                                            AS new_issue_contact_case_ts
-      -- , ARRAY_CONSTRUCT(b1.origin, b2.origin)                                 AS origins
-      , TRUE                                                                  AS has_new_issue_contact
-      , b2.problem_tag                                                        AS new_issue_contact_problem_tag
+      , DATEDIFF(DAY, b1.case_creation_ts_utc, b2.case_creation_ts_utc) AS days_since_new_issue_contact
+      , b2.case_creation_ts_utc                                         AS new_issue_contact_case_ts_utc
+      , TRUE                                                            AS has_new_issue_contact
+      , b2.problem_tag                                                  AS new_issue_contact_problem_tag
     FROM base b1
     JOIN base b2
       ON b1.customer_token = b2.customer_token
@@ -49,11 +33,10 @@ WITH
   , repeat_issue_contact AS (
     SELECT DISTINCT
       b1.case_id
-      , DATEDIFF(DAY, b1.case_creation_date_time, b2.case_creation_date_time) AS days_since_repeat_issue_contact
-      , b2.case_creation_date_time                                            AS repeat_issue_contact_case_ts
-      -- , ARRAY_CONSTRUCT(b1.origin, b2.origin)                                 AS origins
-      , TRUE                                                                  AS has_repeat_issue_contact
-      , b2.problem_tag                                                        AS repeat_issue_contact_problem_tag
+      , DATEDIFF(DAY, b1.case_creation_ts_utc, b2.case_creation_ts_utc) AS days_since_repeat_issue_contact
+      , b2.case_creation_ts_utc                                         AS repeat_issue_contact_case_ts_utc
+      , TRUE                                                            AS has_repeat_issue_contact
+      , b2.problem_tag                                                  AS repeat_issue_contact_problem_tag
     FROM base b1
     JOIN base b2
       ON b1.customer_token = b2.customer_token
@@ -62,40 +45,85 @@ WITH
     QUALIFY
       ROW_NUMBER() OVER (PARTITION BY b1.case_id ORDER BY ABS(days_since_repeat_issue_contact)) = 1
   )
-  , fcr_calculation AS (
-    SELECT
-      b.customer_id
-      , b.customer_token
-      , b.problem_tag
-      , b.case_creation_date_time
-      , b.case_id
-      , ric.days_since_repeat_issue_contact
-      , ric.repeat_issue_contact_case_ts
-      , ric.has_repeat_issue_contact
-      , ric.repeat_issue_contact_problem_tag
-      , nic.days_since_new_issue_contact
-      , nic.new_issue_contact_case_ts
-      , nic.has_new_issue_contact
-      , nic.new_issue_contact_problem_tag
-
-      , IFF(ABS(NVL(ric.days_since_repeat_issue_contact, 100)) > 7, TRUE, FALSE)  AS is_fcr_7_day
-      , IFF(ABS(NVL(ric.days_since_repeat_issue_contact, 100)) > 14, TRUE, FALSE) AS is_fcr_14_day
-      , IFF(ABS(NVL(ric.days_since_repeat_issue_contact, 100)) > 28, TRUE, FALSE) AS is_fcr_28_day
-
-    FROM base b
-    LEFT JOIN new_issue_contact nic
-      ON b.case_id = nic.case_id
-    LEFT JOIN repeat_issue_contact ric
-      ON b.case_id = ric.case_id
-    WHERE
-      1 = 1
-    -- QUALIFY
-    --   COUNT(*) OVER (PARTITION BY b.customer_token) > 1
-    ORDER BY b.customer_token
+  , any_issue_contact AS (
+    SELECT DISTINCT
+      b1.case_id
+      , DATEDIFF(DAY, b1.case_creation_ts_utc, b2.case_creation_ts_utc) AS days_since_any_issue_contact
+      , b2.case_creation_ts_utc                                         AS any_issue_contact_case_ts_utc
+      , TRUE                                                            AS has_any_issue_contact
+      , b2.problem_tag                                                  AS any_issue_contact_problem_tag
+    FROM base b1
+    JOIN base b2
+      ON b1.customer_token = b2.customer_token
+      AND b1.case_id != b2.case_id
+    QUALIFY
+      ROW_NUMBER() OVER (PARTITION BY b1.case_id ORDER BY ABS(days_since_any_issue_contact)) = 1
   )
+-- , fcr_calculation AS (
+SELECT
+  b.case_id
+  , b.customer_token
+  , b.problem_tag
+  , b.case_creation_ts_utc
+  , ric.days_since_repeat_issue_contact
+  , ric.repeat_issue_contact_case_ts_utc
+  , ric.has_repeat_issue_contact
+  , ric.repeat_issue_contact_problem_tag
+  , nic.days_since_new_issue_contact
+  , nic.new_issue_contact_case_ts_utc
+  , nic.has_new_issue_contact
+  , nic.new_issue_contact_problem_tag
+  , aic.days_since_any_issue_contact
+  , aic.any_issue_contact_case_ts_utc
+  , aic.has_any_issue_contact
+  , aic.any_issue_contact_problem_tag
+  , IFF(ABS(NVL(ric.days_since_repeat_issue_contact, 100)) > 7, TRUE, FALSE)  AS is_fcr_7_day
+  , IFF(ABS(NVL(ric.days_since_repeat_issue_contact, 100)) > 14, TRUE, FALSE) AS is_fcr_14_day
+  , IFF(ABS(NVL(ric.days_since_repeat_issue_contact, 100)) > 28, TRUE, FALSE) AS is_fcr_28_day
+FROM base b
+LEFT JOIN new_issue_contact nic
+  ON b.case_id = nic.case_id
+LEFT JOIN repeat_issue_contact ric
+  ON b.case_id = ric.case_id
+LEFT JOIN any_issue_contact aic
+  ON b.case_id = aic.case_id
+WHERE
+  1 = 1
+-- QUALIFY
+--   COUNT(*) OVER (PARTITION BY b.customer_token) > 1
+ORDER BY b.customer_token
+;
+
+-- )
+--
+-- SELECT
+--   1 = 1
+--   , ROUND(AVG(ABS(days_since_any_issue_contact)))                                     AS avg_abs_days_since_any_issue_contact
+--   , MEDIAN(IFF(has_any_issue_contact, ABS(days_since_any_issue_contact), NULL))       AS median_abs_days_since_any_issue_contact
+--
+--   , ROUND(AVG(ABS(days_since_new_issue_contact)))                                     AS avg_abs_days_since_new_issue_contact
+--   , ROUND(AVG(ABS(days_since_repeat_issue_contact)))                                  AS avg_abs_days_since_repeat_issue_contact
+--   , MEDIAN(IFF(has_repeat_issue_contact, ABS(days_since_repeat_issue_contact), NULL)) AS median_abs_days_since_repeat_issue_contact
+--   , ROUND(COUNT(DISTINCT IFF(has_new_issue_contact, customer_token, NULL)) /
+--             COUNT(DISTINCT customer_token) * 100, 2)                                  AS percent_of_new_issue_customers
+--   , ROUND(COUNT(DISTINCT IFF(has_repeat_issue_contact, customer_token, NULL)) /
+--             COUNT(DISTINCT customer_token) * 100, 2)                                  AS percent_of_repeat_issue_customers
+--   , ROUND(COUNT_IF(is_fcr_7_day) / NULLIFZERO(COUNT(DISTINCT case_id)) * 100, 2)      AS total_fcr_7_day
+--   , ROUND(COUNT_IF(is_fcr_14_day) / NULLIFZERO(COUNT(DISTINCT case_id)) * 100, 2)     AS total_fcr_14_day
+--   , ROUND(COUNT_IF(is_fcr_28_day) / NULLIFZERO(COUNT(DISTINCT case_id)) * 100, 2)     AS total_fcr_28_day
+-- FROM fcr_calculation
+-- WHERE
+--   1 = 1
+
+
+DROP TABLE personal_johndarrah.public.fact_voice_fcr
+;
 
 SELECT
   1 = 1
+  , ROUND(AVG(ABS(days_since_any_issue_contact)))                                     AS avg_abs_days_since_any_issue_contact
+  , MEDIAN(IFF(has_any_issue_contact, ABS(days_since_any_issue_contact), NULL))       AS median_abs_days_since_any_issue_contact
+
   , ROUND(AVG(ABS(days_since_new_issue_contact)))                                     AS avg_abs_days_since_new_issue_contact
   , ROUND(AVG(ABS(days_since_repeat_issue_contact)))                                  AS avg_abs_days_since_repeat_issue_contact
   , MEDIAN(IFF(has_repeat_issue_contact, ABS(days_since_repeat_issue_contact), NULL)) AS median_abs_days_since_repeat_issue_contact
@@ -106,8 +134,10 @@ SELECT
   , ROUND(COUNT_IF(is_fcr_7_day) / NULLIFZERO(COUNT(DISTINCT case_id)) * 100, 2)      AS total_fcr_7_day
   , ROUND(COUNT_IF(is_fcr_14_day) / NULLIFZERO(COUNT(DISTINCT case_id)) * 100, 2)     AS total_fcr_14_day
   , ROUND(COUNT_IF(is_fcr_28_day) / NULLIFZERO(COUNT(DISTINCT case_id)) * 100, 2)     AS total_fcr_28_day
-FROM fcr_calculation
+FROM personal_johndarrah.public.fact_voice_fcr
 WHERE
-  1 = 1
+  YEAR(case_creation_date_time_utc) = 2024
 
-
+SELECT *
+FROM personal_johndarrah.public.fact_voice_fcr
+;
