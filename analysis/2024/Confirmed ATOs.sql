b1dd26f564a3b8a106bba717be73f65d819b41d6
John Darrah <132414407+johndarrah@users.noreply.github.com>
2024-05-14 14:28:34 -0700

    update source

--- analysis/2024/Confirmed ATOs.sql
+++ analysis/2024/Confirmed ATOs.sql
@@ -27,13 +27,13 @@ WITH
           WHEN ARRAY_CONTAINS('ato_p2p_escalation'::VARIANT, hashtag_array)
             THEN TRUE
           ELSE FALSE
-        END                                                                                          AS is_confirmed_ato
+        END                                                                                          AS is_confirmed_via_hashtag
       , CASE
           WHEN autolock_or_self_reported = 'Auto-Lock'
-            AND is_confirmed_ato
+            AND is_confirmed_via_hashtag
             THEN 'Confirmed Auto-Lock'
           WHEN autolock_or_self_reported = 'Self Reported'
-            AND is_confirmed_ato
+            AND is_confirmed_via_hashtag
             THEN 'Confirmed Self Reported'
           ELSE NULL
         END                                                                                          AS ato_type
@@ -41,8 +41,11 @@ WITH
     WHERE
       1 = 1
       AND YEAR(hashtag_ts_utc) >= 2024
-    QUALIFY
-      is_confirmed_ato
+      AND hashtag IN ('ATO_INV_LATO',
+                      'ATO_INV_ATO',
+                      'ATO_RESET_CONFIRMED',
+                      'ATO_P2P_ESCALATION',
+                      'ATO_REIMBURSMENT')
   )
   , rollbacks AS (
     SELECT
@@ -70,9 +73,10 @@ WITH
       , r.mass_rollback_type
       , r.rolled_back_to_ts_utc
       , r.roll_back_actioned_ts_utc
+      , ah.hashtag_ts_utc
       , ah.comment_case_number
       , NVL(ah.autolock_or_self_reported, 'No Applicable Hashtag')                 AS autolock_or_self_reported
-      , ah.is_confirmed_ato
+      , ah.is_confirmed_via_hashtag
       , ah.ato_type
       , ah.advocate_ldap
       , ah.hashtag_array
@@ -83,22 +87,89 @@ WITH
     FROM rollbacks r
     LEFT JOIN app_cash_cs.public.support_cases sc
       ON r.customer_token = sc.customer_token
-      AND CONVERT_TIMEZONE('America/Los_Angeles', 'UTC', sc.case_creation_date_time) BETWEEN r.rolled_back_to_ts_utc AND r.roll_back_actioned_ts_utc
+      AND CONVERT_TIMEZONE('America/Los_Angeles', 'UTC', sc.case_creation_date_time)::DATE
+        BETWEEN r.rolled_back_to_ts_utc::DATE AND r.roll_back_actioned_ts_utc::DATE
     LEFT JOIN ato_hashtags ah
       ON r.customer_token = ah.customer_token
-      AND r.roll_back_actioned_ts_utc::DATE = ah.hashtag_ts_utc::DATE
+      AND ah.hashtag_ts_utc::DATE BETWEEN r.rolled_back_to_ts_utc::DATE AND r.roll_back_actioned_ts_utc::DATE
+    -- AND r.roll_back_actioned_ts_utc::DATE = ah.hashtag_ts_utc::DATE
     WHERE
       1 = 1
     QUALIFY
       ROW_NUMBER() OVER (PARTITION BY rollback_id ORDER BY last_assigned_queue = 'Risk ATO' DESC,case_creation_ts_utc) = 1
     ORDER BY ah.comment_case_number
   )
+-- -- counts
+-- SELECT DISTINCT
+--   autolock_or_self_reported
+--   , COUNT(*)
+--   , COUNT(DISTINCT rollback_id)
+-- FROM base
+-- WHERE
+--   1 = 1
+-- GROUP BY 1
+-- ;
 
-SELECT DISTINCT
-  autolock_or_self_reported
-  , COUNT(*) AS total_cases
+
+SELECT DISTINCT *
 FROM base
 WHERE
   1 = 1
-GROUP BY 1
-ORDER BY 2 DESC
+  -- AND customer_token = 'C_80dn4pyz2'
+
+  AND autolock_or_self_reported = 'No Applicable Hashtag'
+
+
+--------------------- Validation for missing hashtags
+-- SELECT DISTINCT
+--   CONVERT_TIMEZONE('America/Los_Angeles', 'UTC', ah.hashtag_date_time)        AS hashtag_ts_utc
+--   -- , ah.comment
+--   -- , ah.hashtag
+--   , ARRAY_AGG(DISTINCT LOWER(ah.hashtag)) OVER (PARTITION BY ah.target_token) AS hashtag_array
+--   , CASE
+--       WHEN ARRAY_CONTAINS('ato_inv_ato'::VARIANT, hashtag_array)
+--         THEN 'Self Reported'
+--       WHEN ARRAY_CONTAINS('ato_inv_lato'::VARIANT, hashtag_array)
+--         THEN 'Auto-Lock'
+--       ELSE 'No Applicable Hashtag'
+--     END                                                                       AS autolock_or_self_reported
+--   , CASE
+--       WHEN ARRAY_CONTAINS('ato_reset_confirmed'::VARIANT, hashtag_array)
+--         THEN TRUE
+--       WHEN ARRAY_CONTAINS('ato_reimbursment'::VARIANT, hashtag_array)
+--         THEN TRUE
+--       WHEN ARRAY_CONTAINS('ato_secured_ffato'::VARIANT, hashtag_array)
+--         THEN TRUE
+--       WHEN ARRAY_CONTAINS('ato_p2p_escalation'::VARIANT, hashtag_array)
+--         THEN TRUE
+--       ELSE FALSE
+--     END                                                                       AS is_confirmed_ato
+--   , r.created_at                                                              AS roll_back_actioned_ts_utc
+--   , r.rolled_back_to
+-- FROM app_cash_cs.public.ato_hashtags ah
+-- JOIN app_cash.app.asset_rollbacks r
+--   ON r.customer_token = ah.target_token
+--   AND r.created_at::DATE = hashtag_ts_utc::DATE
+-- WHERE
+--   1 = 1
+-- -- AND ah.target_token = 'C_5nevrqmzb'
+-- QUALIFY
+--   autolock_or_self_reported = 'Self Reported'
+--   AND NOT is_confirmed_ato
+
+-- -- confirmed atos count alignment
+-- WITH
+--   base AS (
+--     SELECT DISTINCT *
+--     FROM app_cash.app.asset_rollbacks r
+--     WHERE
+--       1 = 1
+--       AND r.mass_rollback_type IS NULL
+--       AND NVL(r.category, '') NOT IN ('FRIENDLY_FRAUD_ATO', 'RAT_ANDROID')
+--       AND YEAR(created_at) >= 2024
+--     QUALIFY
+--       ROW_NUMBER() OVER (PARTITION BY r.customer_id,r.created_at::DATE ORDER BY r.created_at DESC) = 1
+--   )
+-- SELECT
+--   COUNT(*)
+-- FROM base
